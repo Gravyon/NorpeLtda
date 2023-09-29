c43ea3ab151647e359fe4a1ccfd95880b17c5989
Gravyon <rdjmartinez95@gmail.com>
2023-09-29 14:20:32 -0300

    Initial form

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { SliderCarousel } from "../../components/SliderCarousel";
 import Servicios from "../../components/Servicios";
+import ContactForm from "../../components/ContactForm";
 
 const Home = () => {
+  const fields = [
+    { name: "Nombre", label: "Nombre", required: true },
+    { name: "Correo", label: "Correo", type: "email", required: true },
+    { name: "Telefono", label: "Telefono", type: "tel", required: true },
+    { name: "Mensaje", label: "Mensaje", type: "textarea", required: true },
+  ];
   return (
     <div>
       <SliderCarousel />
       <Servicios />
+      <ContactForm
+        color="green"
+        fields={fields}
+        title="Cotiza tu servicio aquí"
+      />
     </div>
   );
 };
