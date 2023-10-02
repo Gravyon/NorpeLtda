b25174bece3d4c749c8117ce69bd281b1d5e44c7
Gravyon <rdjmartinez95@gmail.com>
2023-10-01 22:25:57 -0300

    Form updated

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,66 +1,78 @@
 import React from "react";
+import { useForm } from "@formspree/react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Button } from "@material-tailwind/react";
 
-const ContactForm = ({ color, fields, title }) => {
+const ContactForm = ({ color, fields, title, validation }) => {
+  const [state, handleSubmit] = useForm(import.meta.env.VITE_CONTACT);
   return (
-    <div className={`p-4 ${color === "green" ? "bg-norpe-green" : "bg-white"}`}>
-      <h2
-        className={`text-xl font-semibold ${
+    <div
+      className={`py-12 px-32 ${
+        color === "green" ? "bg-norpe-green" : "bg-gray-100"
+      } `}
+    >
+      <p
+        className={`flex justify-center text-3xl pb-10 font-bold  ${
           color === "green" ? "text-white" : "text-norpe-green"
         }`}
       >
         {title}
-      </h2>
+      </p>
       <Formik
-        initialValues={{}}
-        onSubmit={(values) => {
-          // Handle form submission logic here
-          console.log("Form data submitted:", values);
-        }}
-        validate={(values) => {
-          // Add your validation logic here
-          const errors = {};
-          // Example: Validate required fields
-          fields.forEach((field) => {
-            if (field.required && !values[field.name]) {
-              errors[field.name] = `${field.label} is required`;
-            }
-          });
-          return errors;
-        }}
+        // initialValues={{}}
+        validationSchema={validation}
+        onSubmit={handleSubmit}
       >
-        <Form>
-          {fields.map((field) => (
-            <div key={field.name} className="mb-4">
-              <label
-                htmlFor={field.name}
-                className={`${
-                  color === "green" ? "text-white" : "text-norpe-green"
-                }`}
-              >
-                {field.label}
-              </label>
-              <Field
-                type={field.type || "text"}
-                name={field.name}
-                id={field.name}
-                placeholder={field.placeholder || ""}
-                className={`w-full px-3 py-2 border rounded-md bg-white`}
-              />
-              <ErrorMessage
-                name={field.name}
-                component="div"
-                className="text-red-500 mt-1"
-              />
+        {({ isSubmitting }) => (
+          <Form>
+            <div className="grid grid-rows-3">
+              {" "}
+              {fields.map((field) => (
+                <div key={field.name} className="mb-4">
+                  <label
+                    htmlFor={field.name}
+                    className={`${
+                      color === "green" ? "text-white" : "text-norpe-green"
+                    }`}
+                  >
+                    {field.label}
+                  </label>
+                  {!field.textarea ? (
+                    <Field
+                      type={field.type}
+                      name={field.name}
+                      id={field.name}
+                      className={`block w-full p-2 border rounded-md bg-white`}
+                    />
+                  ) : (
+                    <Field
+                      type="text"
+                      component="textarea"
+                      name="message"
+                      rows="4"
+                      className="block w-full p-2 rounded-md bg-white"
+                    />
+                  )}
+                  <ErrorMessage
+                    name={field.name}
+                    component="div"
+                    className="text-red-500 mt-1"
+                  />
+                </div>
+              ))}
             </div>
-          ))}
-          <button
-            type="submit"
-            className={`bg-${color}-500 text-white px-4 py-2 rounded-md hover:bg-${color}-600`}
-          >
-            Submit
-          </button>
-        </Form>
+
+            <Button
+              size="lg"
+              type="submit"
+              className={`bg-norpe-green ${
+                color === "green" ? "outline outline-1 outline-white" : ""
+              } text-white px-4 py-2 rounded-3xl hover:bg-aquamarine`}
+            >
+              Enviar
+            </Button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
