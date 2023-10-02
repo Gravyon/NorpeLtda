6391e49571e70b5890d1642ecbec7f9ab26ca4f9
Gravyon <rdjmartinez95@gmail.com>
2023-10-02 09:05:19 -0300

    Validation added, added import for formvalues to reduce clutter

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 import ContactForm from "../../components/ContactForm";
 import { whiteForm } from "../../validations/contactSchema";
+import { whiteFields } from "../../helpers/formValues";
 
 const Contact = () => {
-  const fields = [
-    { name: "fname", label: "Nombre:*", textarea: false },
-    { name: "lname", label: "Apellido:*", textarea: false },
-    { name: "topic", label: "Tema:*", textarea: false },
-    { name: "email", label: "Correo:*", type: "email", textarea: false },
-    { name: "phone", label: "Telefono", type: "tel", textarea: false },
-    { name: "message", label: "Mensaje:*", type: "textarea", textarea: true },
-  ];
   return (
     <div>
       <ContactForm
         color="white"
-        fields={fields}
+        fields={whiteFields}
         validation={whiteForm}
         title="ENVIANOS UN MENSAJE"
       />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,21 +3,16 @@ import { SliderCarousel } from "../../components/SliderCarousel";
 import Servicios from "../../components/Servicios";
 import ContactForm from "../../components/ContactForm";
 import { greenForm } from "../../validations/contactSchema";
+import { greenFields } from "../../helpers/formValues";
 
 const Home = () => {
-  const fields = [
-    { name: "fname", label: "Nombre:*", textarea: false },
-    { name: "email", label: "Correo:*", type: "email", textarea: false },
-    { name: "phone", label: "Telefono:", type: "tel", textarea: false },
-    { name: "message", label: "Mensaje:*", type: "text", textarea: true },
-  ];
   return (
     <div>
       <SliderCarousel />
       <Servicios />
       <ContactForm
         color="green"
-        fields={fields}
+        fields={greenFields}
         title="Cotiza tu servicio aquí"
         validation={greenForm}
       />
