139d95fdd1cf595186fb83743db7705614b00f91
Gravyon <rdjmartinez95@gmail.com>
2023-10-02 15:24:10 -0300

    Images added

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import ContactForm from "../../components/ContactForm";
 import { whiteForm } from "../../validations/contactSchema";
 import { whiteFields } from "../../helpers/formValues";
-
+import texto_contacto from "../../assets/texto_contacto.png";
+import contactanos from "../../assets/contactanos.png";
 const Contact = () => {
   return (
     <div>
+      <img src={contactanos} alt="" />
+      <img src={texto_contacto} alt="texto contacto" />
       <ContactForm
         color="white"
         fields={whiteFields}
