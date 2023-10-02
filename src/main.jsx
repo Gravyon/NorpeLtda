628040a965ddadb4039195262794a80ef5ad55d9
Gravyon <rdjmartinez95@gmail.com>
2023-10-02 10:55:43 -0300

    Forms working, still needs to reset values after sending

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { router } from "./router";
 import { RouterProvider } from "react-router-dom";
-
+import { Toaster } from "sonner";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Toaster richColors />
   </React.StrictMode>
 );
