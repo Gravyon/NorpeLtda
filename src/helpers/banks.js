e4a374392a1dbcdceb0af0b145b3ddca4aed7ca8
Gravyon <rdjmartinez95@gmail.com>
2023-10-03 10:00:37 -0300

    Created an exportable file for payment section

---
+++ src/helpers/banks.js
@@ -0,0 +1,13 @@
+import bbva from "../assets/bbva.png";
+import santander from "../assets/santander.png";
+import bundes from "../assets/bundes.png";
+import scotiabank from "../assets/scotiabank.png";
+import republica from "../assets/republica.png";
+
+export const banks = [
+  { name: "republica", image: republica },
+  { name: "scotiabank", image: scotiabank },
+  { name: "bundes", image: bundes },
+  { name: "santander", image: santander },
+  { name: "bbva", image: bbva },
+];
--- src/pages/Payment/PaymentChoice.jsx
+++ src/pages/Payment/PaymentChoice.jsx
@@ -1,19 +1,8 @@
 import React from "react";
-import bbva from "../../assets/bbva.png";
-import santander from "../../assets/santander.png";
-import bundes from "../../assets/bundes.png";
-import scotiabank from "../../assets/scotiabank.png";
-import republica from "../../assets/republica.png";
 import payment2 from "../../assets/payment2.png";
+import { banks } from "../../helpers/banks";
 
 const PaymentChoice = () => {
-  const banks = [
-    { name: "republica", image: republica },
-    { name: "scotiabank", image: scotiabank },
-    { name: "bundes", image: bundes },
-    { name: "santander", image: santander },
-    { name: "bbva", image: bbva },
-  ];
   return (
     <div className="md:flex">
       <div className="flex justify-center">
