b9092aa041c3973337bc5ca4c9aa08d924af1319
Gravyon <rdjmartinez95@gmail.com>
2023-10-03 14:03:44 -0300

    Tips section added

--- src/helpers/videos.js
+++ src/helpers/videos.js
@@ -1,20 +1,50 @@
 export const videos = [
   {
     title: "video1",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita tenetur eos qui aut neque delectus esse! Optio itaque ex, eaque obcaecati dolorum dignissimos vel sapiente adipisci odio? Aspernatur, blanditiis quos!",
+    description: `Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Aliquam consequuntur nobis molestias porro
+              laboriosam voluptate illo consequatur fugiat veniam quod numquam
+              earum, ex dicta quos odit deserunt quaerat rem voluptatum. Lorem,
+              ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+              maxime optio aperiam deserunt dolor impedit ab similique
+              voluptatum illo quia. Maxime voluptas fugit harum aliquam quo quas
+              fuga consequatur repellendus. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Voluptas delectus blanditiis facilis
+              ad, consectetur aliquam? Molestiae cum, pariatur, iste quia soluta
+              voluptatibus fugiat doloremque illum commodi sapiente excepturi ut
+              inventore?`,
     source: "https://www.youtube.com/embed/SYxN134gb-8?si=0MCuxI89-_Dp01KT",
   },
   {
     title: "video2",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita tenetur eos qui aut neque delectus esse! Optio itaque ex, eaque obcaecati dolorum dignissimos vel sapiente adipisci odio? Aspernatur, blanditiis quos!",
+    description: `Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Aliquam consequuntur nobis molestias porro
+              laboriosam voluptate illo consequatur fugiat veniam quod numquam
+              earum, ex dicta quos odit deserunt quaerat rem voluptatum. Lorem,
+              ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+              maxime optio aperiam deserunt dolor impedit ab similique
+              voluptatum illo quia. Maxime voluptas fugit harum aliquam quo quas
+              fuga consequatur repellendus. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Voluptas delectus blanditiis facilis
+              ad, consectetur aliquam? Molestiae cum, pariatur, iste quia soluta
+              voluptatibus fugiat doloremque illum commodi sapiente excepturi ut
+              inventore?`,
     source: "https://www.youtube.com/embed/SYxN134gb-8?si=0MCuxI89-_Dp01KT",
   },
   {
     title: "video3",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita tenetur eos qui aut neque delectus esse! Optio itaque ex, eaque obcaecati dolorum dignissimos vel sapiente adipisci odio? Aspernatur, blanditiis quos!",
+    description: `Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Aliquam consequuntur nobis molestias porro
+              laboriosam voluptate illo consequatur fugiat veniam quod numquam
+              earum, ex dicta quos odit deserunt quaerat rem voluptatum. Lorem,
+              ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+              maxime optio aperiam deserunt dolor impedit ab similique
+              voluptatum illo quia. Maxime voluptas fugit harum aliquam quo quas
+              fuga consequatur repellendus. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Voluptas delectus blanditiis facilis
+              ad, consectetur aliquam? Molestiae cum, pariatur, iste quia soluta
+              voluptatibus fugiat doloremque illum commodi sapiente excepturi ut
+              inventore?`,
     source: "https://www.youtube.com/embed/SYxN134gb-8?si=0MCuxI89-_Dp01KT",
   },
 ];
