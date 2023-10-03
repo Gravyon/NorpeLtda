b4a8389c28792458a5924aeb2694d2f0ee0d64cc
Gravyon <rdjmartinez95@gmail.com>
2023-10-03 11:02:55 -0300

    Added main video

--- src/pages/Tips/Tips.jsx
+++ src/pages/Tips/Tips.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import tips_banner from "../../assets/tips_banner.png";
 import texto_tips from "../../assets/texto_tips.png";
+import VideoSection from "./VideoSection";
 
 export const Tips = () => {
   return (
     <div>
       <img src={tips_banner} alt="Banner de tips" />
       <img src={texto_tips} alt="Texto tips" />
+      <VideoSection />
     </div>
   );
 };
