b95405b4e196fbf03b591235205e688284a3a7dc
Gravyon <rdjmartinez95@gmail.com>
2023-10-03 10:10:30 -0300

    Images added

--- src/pages/Tips/Tips.jsx
+++ src/pages/Tips/Tips.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import tips_banner from "../../assets/tips_banner.png";
+import texto_tips from "../../assets/texto_tips.png";
 
 export const Tips = () => {
-  return <div>Tips</div>;
+  return (
+    <div>
+      <img src={tips_banner} alt="Banner de tips" />
+      <img src={texto_tips} alt="Texto tips" />
+    </div>
+  );
 };
