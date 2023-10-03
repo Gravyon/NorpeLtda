df1a6a6b78bac1649240c8f57a9b28c69a20c000
Gravyon <rdjmartinez95@gmail.com>
2023-10-03 09:58:31 -0300

    Responsiveness added

--- src/pages/Payment/PaymentChoice.jsx
+++ src/pages/Payment/PaymentChoice.jsx
@@ -15,15 +15,22 @@ const PaymentChoice = () => {
     { name: "bbva", image: bbva },
   ];
   return (
-    <div className="flex">
-      <div>
-        <img className="ml-12" src={payment2} alt="Seleccion de pago" />
+    <div className="md:flex">
+      <div className="flex justify-center">
+        <img className="md:ml-12" src={payment2} alt="Seleccion de pago" />
       </div>
-      <div className="p-24">
-        <p className="text-xl font-medium">SELECCIONE SU BANCO: </p>
-        <div className="grid grid-cols-2">
+      <div className="p-10 md:p-24">
+        <p className="hidden md:block text-xl md:mb-6 md:ml-8 text-center md:text-start font-medium">
+          SELECCIONE SU BANCO:{" "}
+        </p>
+        <div className="md:grid md:grid-cols-2">
+          <p className="md:hidden text-xl text-center md:text-start font-medium">
+            SELECCIONE SU BANCO:{" "}
+          </p>
           {banks.map((item) => (
-            <img className="p-2" src={item.image} alt={item.name} srcset="" />
+            <button className="p-2 md:px-8 md:p-4">
+              <img src={item.image} alt={item.name} />
+            </button>
           ))}
         </div>
       </div>
