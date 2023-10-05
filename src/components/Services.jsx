844122ca2d3e151ef4137a6e33f28e24ce11b5ab
Gravyon <rdjmartinez95@gmail.com>
2023-10-05 11:05:45 -0300

    Made services section into an array and fixed the placement of items, also added a way to reverse the order of the content

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -8,105 +8,71 @@ import { Button } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 
 const Services = () => {
+  const sections = [
+    {
+      id: "monitoreo",
+      title: "MONITOREO Y RESPUESTA",
+      description:
+        "El sistema de alarmas enviará señales a nuestro centro de monitoreo con toda la actividad del sistema. Ante eventos de emergencia, uno de nuestros móviles concurrirá al lugar.",
+      imageSrc: monitoreo,
+    },
+    {
+      id: "alarmas",
+      title: "ALARMAS DE ROBO",
+      description:
+        "El sistema dará aviso ante cualquier incidente o posible intrusión de su casa o negocio.",
+      imageSrc: alarmas,
+    },
+    {
+      id: "deteccion",
+      title: "DETECCIÓN DE INCENDIOS",
+      description:
+        "El sistema emite una alerta ante la existencia de fenómenos inherentes al fuego. Todas las instalaciones se realizan acorde a la normativa vigente y con elementos homologados por la DNB.",
+      imageSrc: deteccion,
+    },
+    {
+      id: "cctv",
+      title: "SISTEMAS DE CCTV",
+      description:
+        "Proporciona vigilancia continúa y grabación de cámaras de seguridad implementando las últimas tecnologías en la materia.",
+      imageSrc: cctv,
+    },
+  ];
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col bg-light-gray">
       <img src={servicios} />
-      <div id="monitoreo" className="flex flex-col md:flex-row">
-        <img className="h-80 md:w-1/2" src={monitoreo} />
-        <div className="text-center h-80 w-full sm:w-full md:w-1/2 bg-light-gray">
-          <div className="text-2xl mt-10 font-medium text-norpe-green">
-            <strong>MONITOREO Y RESPUESTA</strong>
-          </div>
-          <div className="text-justify mt-5 mx-24 xl:mb-12 md:mb-12 sm:mb-0 text-xl text-gray-500">
-            El sistema de alarmas enviará señales a nuestro centro de monitoreo
-            con toda la actividad del sistema. Ante eventos de emergencia, uno
-            de nuestros móviles concurrirá al lugar.
-          </div>
-          <Link to="/contacto">
-            {" "}
-            <Button
-              size="lg"
-              className="bg-norpe-green mb-5 hover:bg-aquamarine rounded-3xl"
-              variant="filled"
-            >
-              SOLICITAR MÁS DETALLES
-            </Button>
-          </Link>
-        </div>
-      </div>
-      <div className="flex flex-col md:flex-row">
+      {sections.map((section, index) => (
         <div
-          id="alarmas"
-          className="text-center h-80 w-full sm:w-full md:w-1/2 bg-light-gray"
+          key={section.id}
+          // Revierte el orden del contenido
+          className={`flex flex-col md:flex-row ${
+            index % 2 !== 0 ? "md:flex-row-reverse" : ""
+          }`}
         >
-          <div className="text-2xl mt-10 font-medium text-norpe-green">
-            <strong>ALARMAS DE ROBO</strong>
+          <img className="h-96 aspect-video" src={section.imageSrc} />
+          <div className="flex-grow text-center w-full sm:w-full md:w-1/2">
+            <div className="flex flex-col h-full">
+              <div className="text-2xl mt-10 font-medium text-norpe-green">
+                <strong>{section.title}</strong>
+              </div>
+              <div className="grid place-content-center text-justify mx-24 text-xl text-gray-500 flex-grow">
+                {section.description}
+              </div>
+              <div className="flex justify-center items-end m-4">
+                <Link to="/contacto">
+                  <Button
+                    size="lg"
+                    className="bg-norpe-green hover:bg-aquamarine rounded-3xl"
+                    variant="filled"
+                  >
+                    SOLICITAR MÁS DETALLES
+                  </Button>
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className="text-justify mt-5 mx-24 xl:mb-12 md:mb-12 sm:mb-0 text-xl text-gray-500">
-            El sistema dará aviso ante cualquier incidente o posible intrusión
-            de su casa o negocio.
-          </div>
-          <Link to="/contacto">
-            <Button
-              size="lg"
-              className="bg-norpe-green mt-5 hover:bg-aquamarine rounded-3xl"
-              variant="filled"
-            >
-              SOLICITAR MÁS DETALLES
-            </Button>
-          </Link>
         </div>
-        <img className="h-80 md:w-1/2" src={alarmas} />
-      </div>
-      <div className="flex flex-col md:flex-row">
-        <img className="h-80 md:w-1/2" src={deteccion} />
-        <div
-          id="deteccion"
-          className="text-center h-80 w-full sm:w-full md:w-1/2 bg-light-gray"
-        >
-          <div className="text-2xl mt-10 font-medium text-norpe-green">
-            <strong>DETECCIÓN DE INCENDIOS</strong>
-          </div>
-          <p className="text-justify mt-5 mx-24 xl:mb-12 md:mb-12 sm:mb-0 text-xl text-gray-500">
-            El sistema emite una alerta ante la existencia de fenómenos
-            inherentes al fuego. Todas las instalaciones se realizan acorde a la
-            normativa vigente y con elementos homologados por la DNB.
-          </p>
-          <Link to="/contacto">
-            <Button
-              size="lg"
-              className="bg-norpe-green mb-5 hover:bg-aquamarine rounded-3xl"
-              variant="filled"
-            >
-              SOLICITAR MÁS DETALLES
-            </Button>
-          </Link>
-        </div>
-      </div>
-      <div className="flex flex-col md:flex-row">
-        <div
-          id="cctv"
-          className="text-center h-80 w-full sm:w-full md:w-1/2 bg-light-gray"
-        >
-          <div className="text-2xl mt-10 font-medium text-norpe-green">
-            <strong>SISTEMAS DE CCTV</strong>
-          </div>
-          <div className="text-justify mt-5 mx-24 xl:mb-12 md:mb-12 sm:mb-0 text-xl text-gray-500">
-            Proporciona vigilancia continúa y grabación de cámaras de seguridad
-            implementando las últimas tecnologías en la materia.
-          </div>
-          <Link to="/contacto">
-            <Button
-              size="lg"
-              className="bg-norpe-green mb-5 hover:bg-aquamarine rounded-3xl"
-              variant="filled"
-            >
-              SOLICITAR MÁS DETALLES
-            </Button>
-          </Link>
-        </div>
-        <img className="h-80 md:w-1/2" src={cctv} />
-      </div>
+      ))}
     </div>
   );
 };
