5362c3d41f98544ded5df1c7d143064e6069c1f3
Gravyon <rdjmartinez95@gmail.com>
2023-10-05 10:37:10 -0300

    Cleaned up help section

---
+++ src/helpers/helpInfo.js
@@ -0,0 +1,18 @@
+import visita from "../assets/visita.png";
+import llamanos from "../assets/llamanos.png";
+import correo from "../assets/correo.png";
+export const helpInfo = [
+  {
+    title: "VISITANOS EN NUESTRA OFICINA",
+    text: "18 de Julio 509 Colonia del Sacramento URUGUAY",
+    imageSrc: visita,
+  },
+
+  {
+    title: "ENVIANOS UN EMAIL",
+    text: `Escríbenos por correo electrónico haciendo clic aquí`,
+
+    imageSrc: correo,
+  },
+  { title: "LLÁMANOS", text: "+598 4522 4413", imageSrc: llamanos },
+];
